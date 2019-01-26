fed7c1965d796ead0411b81061d98a3024c90ee6
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2019-01-26 11:54:57 +0900

    Update YgoLink.js

--- YgoLink.js
+++ YgoLink.js
@@ -6,8 +6,8 @@ function excludeInput(){
 	for(i=0;i<elem.length;i++)elem[i].value=elem[i].value.replace(/《<a href.+?>(.+?)<\/a>》/g,'《$1》');
 }
 window.onload=function(){
-	document.body.innerHTML=document.body.innerHTML.replace(/《([^_!].*?)》/g,'《<a href="http://yugioh-wiki.net/kamaseinu.cgi?$1" target="_blank">$1</a>》');
-	document.body.innerHTML=document.body.innerHTML.replace(/《!(.*?)》/g,'《<a href="http://yugioh-wiki.net/kamaseinu.cgi?$1">$1</a>》');
-	document.body.innerHTML=document.body.innerHTML.replace(/《_(.*?)》/g,'《$1》');
+	document.body.innerHTML=document.body.innerHTML.replace(/《([^_!].*?)》/g,'《<a href="http://yugioh-wiki.net/kamaseinu.cgi?$1" target="_blank">$1</a>》')
+	.replace(/《!(.*?)》/g,'《<a href="http://yugioh-wiki.net/kamaseinu.cgi?$1">$1</a>》')
+	.replace(/《_(.*?)》/g,'《$1》');
 	excludeInput();
 }
