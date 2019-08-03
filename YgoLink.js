6fa066f93815092b2d02eff623b010c24307fc02
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2019-08-04 06:27:25 +0900

    Update YgoLink.js

--- YgoLink.js
+++ YgoLink.js
@@ -9,6 +9,6 @@ window.onload=function(){
 	document.body.innerHTML=document.body.innerHTML.replace(/《([^_＿!！].*?)》/g,'《<a href="http://yugioh-wiki.net/kamaseinu.cgi?$1" target="_blank">$1</a>》')
 	.replace(/《[!！](.*?)》/g,'《<a href="http://yugioh-wiki.net/kamaseinu.cgi?$1">$1</a>》')
 	.replace(/《[_＿](.*?)》/g,'《$1》')
-	.replace(/kamaseinu\.cgi\?融合/g,'index.php?%A1%D4%CD%BB%B9%E7%A1%D5');
+	.replace(/kamaseinu\.cgi\?融合"/g,'index.php?%A1%D4%CD%BB%B9%E7%A1%D5');
 	excludeInput();
 }
