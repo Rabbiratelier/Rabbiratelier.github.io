8286e90b2afca09d6df9c904b2c44a38256368e5
Mafuyu Kinoshita <36875839+Rabbiratelier@users.noreply.github.com>
2025-10-21 18:29:55 +0900

    Fix '&' character replacement in YgoLink.js
    Added a replacement for '&' character in the onload function.

--- YgoLink.js
+++ YgoLink.js
@@ -15,7 +15,9 @@ window.onload=function(){
 		s = s.replace(/[A-Za-z0-9]/g, function(a) {return String.fromCharCode(a.charCodeAt(0) + 0xFEE0);})
 		return '《<a href="https://rabbiratelier.github.io/YgoLink.html??《' + s.replace(/[　]/g,' ') + '》">' + s + '</a>》';
 	})
-	.replace(/《[_＿](.*?)》/g,'《$1》');
+	.replace(/《[_＿](.*?)》/g,'《$1》')
+	.replace(/+/g,' ');
 
 	excludeInput();
 }
+
